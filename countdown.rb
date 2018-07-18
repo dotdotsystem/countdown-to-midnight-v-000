047b3d13c6d93186d7b6a98ffb0165641d76941d
Claire Harmer <claire.f.harmer@gmail.com>
2018-07-18 21:08:56 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -5,13 +5,14 @@ second = 10
 def countdown(second)
   while second > 0
     puts "#{second} SECOND(S)!"
+
   second -= 1
-    end
-  return "HAPPY NEW YEAR!"
+  end
+    return "HAPPY NEW YEAR!"
 end
 
 def countdown_with_sleep(second)
-  counter = 0
+  second = 0
   while second < 5
   second += 1
   sleep(5)
